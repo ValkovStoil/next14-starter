fab8bf4b7f61dfd625a158c6f6ee2737cef763bb
Stoil Valkov <s.valkov@next-it.bg>
2024-09-18 15:36:54 +0300

    1:26:05 tutorial time

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,44 @@
+import Image from "next/image"
+import styles from "./about.module.css"
+
+
 const AboutPage = () => {
     return(
-        <div>AboutPage</div>
+        <div className={styles.container}>
+            <div className={styles.textContainer}>
+                <h2 className={styles.subtitle}>About Aganecy</h2>
+                <h1 className={styles.title}>We create digital ideas that are bigger, braver, and better.</h1>
+                <p className={styles.desc}>
+                    We create digital ideas that are bigger, braver, and better. We
+                    belive in good ideas, flexibility and precission. We`re the world`s Our
+                    Special Team best consulting & finance solution provider. Wide range
+                    of web and software development services.
+                </p>
+                <div className={styles.boxes}>
+                <div className={styles.box}>
+                        <h1>10 K+</h1>
+                        <p>
+                            Years of experience
+                        </p>
+                    </div>
+                    <div className={styles.box}>
+                        <h1>10 K+</h1>
+                        <p>
+                            Years of experience
+                        </p>
+                    </div>
+                    <div className={styles.box}>
+                        <h1>10 K+</h1>
+                        <p>
+                            Years of experience
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.imgContainer}>
+                <Image src="/about.png" alt="About Image" fill className={styles.img}/>
+            </div>
+        </div>
     )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,11 @@
+import styles from "./footer.module.css"
+
 const Footer = () =>{
     return(
-        <div>Footer</div>
+        <div className={styles.container}>
+            <div className={styles.logo}>Stoil</div>
+            <div className={styles.text}>Lama creative thoughts agency &reg; All rights reserved.</div>
+        </div>
     )
 }
 
